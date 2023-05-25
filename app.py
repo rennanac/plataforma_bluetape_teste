dc546a01abdb8e453efc430d3fec0cfb8af0fa96
rennanac <68173230+rennanac@users.noreply.github.com>
2023-05-25 14:25:42 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -239,38 +239,6 @@ try:
 
             AgGrid(df_resultado)
 
-            ''' gb = GridOptionsBuilder.from_dataframe(df_resultado)
-
-
-            gb.configure_column("DOWNLOAD",
-                                headerName="DOWNLOAD",
-                                cellRenderer=JsCode(
-                                    """
-                                    function(params) {
-                                        if (params.value != ''){
-                                            return '<a href=' + params.value + '> 💾 </a>'
-                                        }else{
-                                            return 'Donwload indisponível.'
-                                        }
-                                    }
-                                    """)
-                                )
-
-            change_color = JsCode("""
-                                  function(params){
-                                    if(params.data.RESULTADO == 'ERRO'){
-
-                                           return {
-                                                'color':'#FF0000'
-                                           }
-                                   }           
-                                   }
-                        """)
-
-            gridOptions = gb.build()
-            gridOptions['getRowStyle'] = change_color
-
-            AgGrid(df_resultado, gridOptions=gridOptions, allow_unsafe_jscode=True, fit_columns_on_grid_load=True)'''
 
             st.subheader("Detalhes do Resultado BB Movimentações: ")
             # table
